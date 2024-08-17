3cc59c4c00890c065fa8aaec4cab4609128ec2e3
DanaMedany <DanaMedany94@gmail.com>
2024-08-17 16:03:32 +0400

    Build a Simple Company Website with React

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        padding: "10px",
+        backgroundColor: "#333",
+        color: "#fff",
+        textAlign: "center",
+      }}
+    >
+      <p>&copy; 2024 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
