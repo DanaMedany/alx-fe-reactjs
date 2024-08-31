5968eab48406864b001c6e081e319759b76ba93e
DanaMedany <DanaMedany94@gmail.com>
2024-08-31 11:04:46 +0400

    .

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import Login from "./components/Login";
-import BlogPost from "./BlogPost";
-import ProtectedRoute from "./ProtectedRoute";
+import BlogPost from "./components/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
