7fc73e6e2d98c761291c471ecdc07e12f129edbf
DanaMedany <DanaMedany94@gmail.com>
2024-09-22 08:34:38 +0400

    .

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import SearchInput from "./components/SearchInput";
+import UserResults from "./components/UserResults";
 
 function App() {
   return (
     <>
       <div className="App">
         <h1>GitHub User Search</h1>
-        {/* Placeholder for Search and Results Components */}
+        <SearchInput />
+        <UserResults />
       </div>
     </>
   );
